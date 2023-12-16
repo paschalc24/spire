4adaf1b46a95b501213ef23ba1c16698b8c58e2c
paschalc24 <pcorrigan@umass.edu>
2023-12-16 02:38:32 -0500

    Refactored course track and got rid of dep store.

--- client/src/components/CourseTrack.svelte
+++ client/src/components/CourseTrack.svelte
@@ -1,84 +1,55 @@
 <script>
-  export let student;
   import StageButton from "./StageButton.svelte";
-  import { DepartmentStore } from "../routes/stores.js";
+  import { StudentStore } from "../routes/stores.js";
 
-  let departments = {};
-  let selectedDepartmentId; 
+  let student;
+  let selectedDepartmentId = null; // Initialize as null
+  let selectedDepartment = {};
 
-  $: selectedDepartment = departments[selectedDepartmentId]; 
-
-  
-  $: if (student && Array.isArray(student.departments)) {
-    fetchDepartments(student.departments);
-  }
-
-  async function fetchDepartments(departmentIds) {
-    let _departments = {};
-    for (const departmentId of departmentIds) {
-      const res = await fetch(`http://localhost:4001/department/${departmentId}`);
-      const data = await res.json();
-      _departments[departmentId] = data;
-    }
-    DepartmentStore.set(_departments);
-
-    // Set the default selected department to the first in the list
-    selectedDepartmentId = departmentIds[0];
-  }
-  
-
-  DepartmentStore.subscribe((_departments) => {
-    departments = _departments;
-    // Reactively update the selected department
-    if (selectedDepartmentId) {
-      selectedDepartment = departments[selectedDepartmentId];
+  // Subscribe to the StudentStore and initialize the data
+  StudentStore.subscribe((_student) => {
+    student = _student;
+    if (student?.departments?.length > 0) {
+      // Initialize selectedDepartment if there are departments available
+      selectedDepartmentId = selectedDepartmentId ?? student.departments[0];
+      selectedDepartment = student.track?.[selectedDepartmentId] ?? {};
     }
   });
 
   // Handle department change
   function onSelectDepartmentChange(event) {
     selectedDepartmentId = event.target.value;
+  
+    // Safely update selectedDepartment with checks
+    selectedDepartment = student.track?.[selectedDepartmentId] ?? {};
   }
 </script>
 
-{#if Object.keys(departments).length > 0}
-  <div class="container-fluid">
-    <label for="department-select" class="degree-header"></label>
-    <select class="department-select h2 border-0 font-weight-light" id="department-select" bind:value={selectedDepartmentId} on:change={onSelectDepartmentChange}>
-      {#each Object.keys(departments) as departmentId (departmentId)} 
-        <option value={departmentId}>{departments[departmentId].title}</option>
-      {/each}
-    </select>
-  </div>
+{#if student?.departments?.length > 0}
+<div class="container-fluid">
+  <select class="h2 department-select" id="department-select" bind:value={selectedDepartmentId} on:change={onSelectDepartmentChange}>
+    {#each student.departments as departmentId (departmentId)}
+      <option value={departmentId}>{departmentId}</option>
+    {/each}
+  </select>
+</div>
 {/if}
 
-{#if selectedDepartment}
-  <div class="container-fluid">
-    <div class="container-fluid p-5">
-      <div class="row">
-        <div class="col">
-          <div class="row m-5">
-            {#each Object.entries(selectedDepartment.track) as [trackKey, trackValue], trackIndex (trackKey)}
-              <StageButton title={trackValue.category} courseIDs={trackValue.courseIDs}/>
-              {#if trackIndex < Object.keys(selectedDepartment.track).length - 1}
-                <center>↓</center>
-              {/if}
-            {/each}
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-{:else}
-  <div class="container-fluid">
-    <h2 class="department-select container-fluid">
-      No departments found
-    </h2>
-  </div>
+{#if Object.keys(selectedDepartment).length > 0}
+<div class="container-fluid">
+  <!-- Display track information for the selected department -->
+  {#each Object.entries(selectedDepartment?.track ?? {}) as [categoryId, category], index}
+    <StageButton title={category.category} courseIDs={category.courseIDs} />
+    {#if index < Object.keys(selectedDepartment.track).length - 1}
+    <center>↓</center>
+    {/if}
+  {/each}
+</div>
 {/if}
 
 <style>
   .department-select {
     font-weight: 100;
+    border: none;
   }
 </style>
--- client/src/routes/stores.js
+++ client/src/routes/stores.js
@@ -1,5 +1,4 @@
 import { writable } from 'svelte/store';
 
 export const StudentStore = writable({});
-export const DepartmentStore = writable({});
 export const CartStore = writable([]);
