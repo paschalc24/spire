58508ef342bc471b59659a3981eb9f0cdba53991
paschalc24 <pcorrigan@umass.edu>
2023-12-16 02:38:54 -0500

    Simplified app and moved on mount to student info.

--- client/src/components/StudentInfoContainer.svelte
+++ client/src/components/StudentInfoContainer.svelte
@@ -1,7 +1,22 @@
 <script>
     import testImage from "../../src/test.png";
     import StudentInfo from "./StudentInfo.svelte";
-    export let student = {};
+    import { onMount } from "svelte";
+    import { StudentStore } from "../routes/stores.js";
+    export let studentId;
+    let student = {};
+
+    onMount(async () => {
+        const studentRes = await fetch(`http://localhost:4004/students/${studentId}`);
+        const studentData = await studentRes.json();
+        console.log("ON MOUNT APP", studentData)
+        StudentStore.set(studentData);
+    });
+
+    StudentStore.subscribe(async (_student) => {
+        console.log(`Student List Subscribe: ${JSON.stringify(_student)}`);
+        student = _student;
+    });
 
 </script>
 
--- client/src/routes/App.svelte
+++ client/src/routes/App.svelte
@@ -2,24 +2,10 @@
     import Nav from "../components/Nav.svelte";
     import StudentInfoContainer from "../components/StudentInfoContainer.svelte";
     import CourseTrack from "../components/CourseTrack.svelte";
-    import { onMount } from "svelte";
-    import { StudentStore } from "./stores.js";
     let studentId = 32417468; //Assuming we have this information configured from authentication
-    let student = {};
-
-    onMount(async () => {
-        const studentRes = await fetch(`http://localhost:4000/student/${studentId}`);
-        const studentData = await studentRes.json();
-        StudentStore.set(studentData);
-    });
-
-    StudentStore.subscribe(async (_student) => {
-        console.log(`Student List Subscribe: ${JSON.stringify(_student)}`);
-        student = _student;
-    });
 
 </script>
 
 <Nav studentId={studentId}/>
-<StudentInfoContainer student={student} />
-<CourseTrack student={student} />
+<StudentInfoContainer studentId={studentId}/>
+<CourseTrack />
